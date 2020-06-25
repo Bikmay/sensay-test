8f897c975d6325b9df9644e0d0b20afdd324ef18
bikmay <artyom.ivliev@gmail.com>
2020-06-25 12:34:30 +0400

    Доделан визуал формы логина

--- src/components/login-page/form/Button.js
+++ src/components/login-page/form/Button.js
@@ -6,14 +6,20 @@ import styles from '../../../styles/login-page/Button.css';
 class Button extends Component {
     constructor(props){
         super(props);
-        this.state={buttonContent: <p className={styles.buttonText}>Войти</p>}
+        this.state={buttonContent: <p className={styles.buttonText}>Войти</p>};
         this.buttonContentLoading = <div className={styles.buttonLoader}></div>;
         this.buttonContentText = <p className={styles.buttonText}>Войти</p>;
+        this.onButtonClick=this.onButtonClick.bind(this);
+    }
+
+    onButtonClick(){
+        this.setState({buttonContent: <div className={styles.buttonLoader}></div>})
+        setTimeout(() => {this.setState({buttonContent: <p className={styles.buttonText}>Войти</p>}); }, 2000);
     }
 
     render() {
         return (
-             <div className={styles.button} onClick>
+             <div className={styles.button} onClick={this.onButtonClick}>
                  {this.state.buttonContent}
              </div>
             
